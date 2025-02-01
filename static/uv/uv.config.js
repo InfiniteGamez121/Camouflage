7fc8cfbf960c3d468b62d04da4350c4f9af20297
Orion codes 3 <the55thone@gmail.com>
2025-01-31 17:38:33 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/static/learn/',
-    bare:'bare-sigma.yourmom.eu.org'',
+    bare: 'bare-sigma.yourmom.eu.org',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
     bundle: '/static/uv/uv.bundle.js',
-    config: '/staticuv/uv.config.js',
+    config: '/static/uv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 };
